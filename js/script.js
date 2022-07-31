20a8948b6264268b73255b7971fdce6f9c18359e
chromium7 <christofersaputra29@gmail.com>
2022-07-31 18:48:34 +0700

    Initial commit

--- js/script.js
+++ js/script.js
@@ -1,42 +1,15 @@
-// Open nav bar in mobile
+const containers = document.querySelectorAll('.container');
+const menuItems = document.querySelectorAll('.menu a');
 
-const menu = document.querySelector(".menu");
-menu.addEventListener('click', () => {
-    openMenu(menu);
-})
-
-function openMenu(menu) {
-    if (menu.classList.contains('change')) {
-        document.querySelector('nav ul').style.transform = 'translateX(90vw)';
-    } else {
-        document.querySelector('nav ul').style.transform = 'none';
-    }
-    menu.classList.toggle("change");
-}
-
-// Navigation links
-
-const navigations = document.querySelectorAll('nav ul li a');
-const sections = document.querySelectorAll('section');
-
-navigations.forEach(nav => {
-    nav.addEventListener('click', () => {
-        if (menu.style.display !== '') {
-            openMenu(menu);
+menuItems.forEach((item) => {
+    item.addEventListener('click', () => {
+        const activeContainer = document.querySelector('.container:not(.hide)');
+        if (activeContainer.id == item.dataset.target) {
+            return;
         }
+
+        activeContainer.classList.add('hide');
+        const target = document.getElementById(item.dataset.target);
+        target.classList.remove('hide');        
     })
 })
-
-// Change navigation links state when scrolling
-
-function changeLinkState() {
-    let index = sections.length;
-
-    while (--index && window.scrollY + 50 < sections[index].offsetTop) {}
-
-    navigations.forEach(nav => nav.classList.remove('active'));
-    navigations[index].classList.add('active');
-}
-
-changeLinkState();
-window.addEventListener('scroll', changeLinkState);
